74955d60b08d124f3df5dd1dfe78d6c7c868775b
manish8kumar <manish80589@gmail.com>
2023-09-01 00:52:10 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,176 @@
+export const Bio = {
+  name: "Manish Kumar",
+  roles: [
+    "Software Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/manish8kumar",
+  resume:
+    "https://drive.google.com/file/d/14Q4bnBurccarRVASatpcoVesZe8h6Nj9/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/manish8kumar/",
+  insta: "https://www.instagram.com/manish_sarswat__/",
+  facebook: "https://www.facebook.com/manish.sarswat.982",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  // {
+  //   id: 1,
+  //   img: "https://media.licdn.com/dms/image/D560BAQFDUKPSU8P0Lw/company-logo_200_200/0/1687947132305?e=1698278400&v=beta&t=cBF3SdszRLspmyvV-rHa1y4tdj_hjjivH7Xy0Z7kOD8",
+  //   role: "Fullstack Externship",
+  //   company: "Flipr",
+  //   date: "June 2023 - July 2023",
+  //   desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+  //   skills: [
+  //     "ReactJS",
+  //     "Redux",
+  //     "NodeJs",
+  //     "Material UI",
+  //     "HTML",
+  //     "CSS",
+  //     "JavaScript",
+  //     "Docker",
+  //     "AWS",
+  //     "MongoDB",
+  //   ],
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://images.shiksha.com/mediadata/images/1563535702phpjTXHxf.jpg",
+    school: "Indian Institute of Information,Guwahati",
+    date: "Dec 2020 - May 2024",
+    grade: "7.12 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Indian Institute of Information,Guwahati. I have completed 4 semesters and have a CGPA of 7.12 . I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    school: "Bhopalwala Arya Sen. Sec. School,Sriganganagar",
+    date: "Apr 2018 - Apr 2019",
+    grade: "87.6%",
+    desc: "I completed my class 12 high school education at Bhopalwala Arya Sen. Sec. School,Sriganganagar where I studied Science with Physics,Chemistry,Maths.",
+  },
+  {
+    id: 2,
+    school: "Little Star Children Sen. Sec. School,Kesrisinghpur",
+    date: "Apr 2016 - Apr 2017",
+    grade: "87.5%",
+    desc: "I completed my class 10 education at Little Star Children Sen. Sec. School,Kesrisinghpur",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "CurrentAffairs",
+    date: "Jun 2022 - Jul 2022",
+    description:
+      "Spearheaded the end-to-end development of a dynamic news website using HTML, CSS, and JavaScript, ensuring responsive design and optimal user experience across various devices. Successfully integrated external APIs to fetch and display real-time news data, showcasing technical skills in JavaScript and enhancing the website’s functionality by providing users with up-to-date and relevant information",
+    img:
+      "https://raw.githubusercontent.com/manish8kumar/CurrentAffairs/main/assets/logo.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/manish8kumar/CurrentAffairs",
+    // webapp: "#",
+  },
+  {
+    id: 0,
+    title: "Pandemic Management System",
+    date: "Jan 2023 - Apr 2023",
+    description:
+      " A web site that provide knowledge about Your Vaccination Slot and You Can test your Vaccination Status with guidelines. Implemented the frontend features and backend server using MongoDB, ExpressJS,React-Js, NodeJS and Jwt Token (MERN Website)",
+    img:
+      "https://raw.githubusercontent.com/saurabh175/Pendmic-management-System/main/client/src/images/covid-2.jpg",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/saurabh175/Pendmic-management-System",
+    //webapp: "#",
+    member: [
+      {
+        name: "Manish Kumar",
+        img: "https://avatars.githubusercontent.com/u/105720449?v=4",
+        linkedin: "https://www.linkedin.com/in/manish8kumar/",
+        github: "https://github.com/manish8kumar/",
+      },
+      {
+        name: "Saurabh Kumar Pandey",
+        img: "https://avatars.githubusercontent.com/u/80593478?v=4",
+        linkedin: "https://www.linkedin.com/in/saurabh-pandey-596905204/",
+        github: "https://github.com/saurabh175",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [];
