5407e25fd91511b88a2499515d3d7c9177a24064
manish8kumar <manish80589@gmail.com>
2023-09-01 15:54:38 +0530

    profile pic  updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/manish8kumar",
   resume:
-    "https://drive.google.com/file/d/14Q4bnBurccarRVASatpcoVesZe8h6Nj9/view?usp=drive_link",
+    "https://drive.google.com/file/d/1Ic1jnp_4rGs10jehHtwX9E7tfTl5VsEu/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/manish8kumar/",
   insta: "https://www.instagram.com/manish_sarswat__/",
   facebook: "https://www.facebook.com/manish.sarswat.982",
